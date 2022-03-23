6c671c3564e6e7be3d1ba5e5fd02a75770617bf5
Carlos Muñoz Díaz <47306703G@cchs.csic.es>
2022-03-23 16:29:35 +0100

    Cambio en datos

--- pre/js/charts/charts.js
+++ pre/js/charts/charts.js
@@ -40,30 +40,48 @@ export function initChart(iframe) {
                 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
         let x = d3.scaleLinear()
-            .domain([-600000,600000])
+            .domain([-1500000,1500000])
             .range([0,width]);
-
-        let xM = d3.scaleLinear()
-            .domain([600000,0])
-            .range([0, width / 2]);
-
-        let xF = d3.scaleLinear()
-            .domain([0,600000])
-            .range([width / 2, width]);
 
         svg.append("g")
             .attr("transform", "translate(0," + height + ")")
             .call(d3.axisBottom(x));
 
         let y = d3.scaleBand()
-                .range([ 0, height ])
-                .domain(d3.map(data, function(item) {console.log(item); return item.Edad; }))
-                .padding(.1);
+            .range([ 0, height ])
+            .domain(data.map(function(item) { return item.Edad; }).reverse())
+            .padding(.1);
 
         svg.append("g")
             .call(d3.axisLeft(y));
 
+        let prueba = ['No_aplicable_hombres_abs','Superiores_hombres_abs','Secundarios_hombres_abs','Primarios_hombres_abs','Analfabetos_hombres_abs','Analfabetos_mujeres_abs','Primarios_mujeres_abs','Secundarios_mujeres_abs','Superiores_mujeres_abs','No_aplicable_mujeres_abs']
+
+        let stackedData = d3.stack()
+            .keys(prueba)
+            (data);
+
+        let color = d3.scaleOrdinal()
+            .domain(prueba)
+            .range([COLOR_GREY_1, COLOR_PRIMARY_1, COLOR_COMP_2, COLOR_COMP_1, COLOR_OTHER_1, COLOR_OTHER_1, COLOR_COMP_1, COLOR_COMP_2, COLOR_PRIMARY_1, COLOR_GREY_1]);
+
+        console.log(stackedData);
+
         function init() {
+            svg.append("g")
+                .selectAll("g")
+                .data(stackedData)
+                .enter()
+                .append("g")
+                .attr("fill", function(d) { return color(d.key); })
+                .selectAll("rect")
+                .data(function(d) { return d; })
+                .enter()
+                .append("rect")
+                .attr("height", y.bandwidth())
+                .attr("y", function(d) { return y(d.data.Edad); })
+                .attr("x", function(d) { return x(d[0]); })
+                .attr("width", function(d) { return x(d[1]) - x(d[0]); });
 
         }
 
